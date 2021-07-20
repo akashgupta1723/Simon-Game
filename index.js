e5a49af6d7bfcadc2729f616c07fc82aea58800e
akashgupta1723 <86875602+akashgupta1723@users.noreply.github.com>
2021-07-20 19:05:50 +0530

    Add initial simon game website

---
+++ index.js
@@ -0,0 +1,79 @@
+var userClickedPattern=[];
+var gamePattern=[];
+var buttonColours=["red", "blue", "green", "yellow"];
+var level=0;
+$(document).keypress(function(){
+  if(gamePattern.length===0)
+  {
+    $("h1").text("Level "+level);
+    nextSequence();
+  }
+});
+
+$(".btn").click(function(){
+  var userChosenColour=$(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+
+      if (userClickedPattern.length === gamePattern.length){
+
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+
+    }
+
+}
+
+
+function nextSequence()
+{
+  userClickedPattern=[];
+  level++;
+  $("h1").text("Level "+level);
+  var randomNumber=Math.floor(Math.random()*4);
+  var randomChosenColour=buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+}
+ function playSound(name)
+ {
+   var audio=new Audio("sounds/"+name+".mp3");
+   audio.play();
+ }
+ function animatePress(currentColour)
+ {
+   $("#"+currentColour).addClass("pressed");
+   setTimeout(function () {
+     $("#" + currentColour).removeClass("pressed");
+   },100);
+ }
